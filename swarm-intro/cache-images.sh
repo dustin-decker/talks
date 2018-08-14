27757b4b44c10cfd597c1c705851280a91cf4f59
Dustin <dustindecker@protonmail.com>
2018-08-13 21:48:12 -0500

    cache images

---
+++ swarm-intro/cache-images.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+. ./resources/lib.sh
+clear
+
+# put your demo awesomeness below here
+
+p "# thanks for pre-caching your images, that should help speed it up during the presentation for those who don't"
+docker pull nicolaka/netshoot
+docker pull golang:1.10-alpine3.8
+docker pull ubuntu
